4799f5ea61a8f8b8cdf24b8fd617d0512cfe11d4
Supremeah <agerskov1994@gmail.com>
2020-09-16 17:23:53 +0200

    GPS emulation introduction
    Introduction of basic GPS emulation. Output format and satelite theory tbd.

---
+++ AVA/Assets/Scripts/Sensors/GPS.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPS
+{
+    // Variables of the class
+    private Vector3 coordinates;
+    private Quaternion orientation;
+    private float accuracy;
+    public float updateFrequency;
+    private float time_real;
+    private int[,] signalMap;
+    private float sateliteStrength;
+
+    private Transform transform;
+
+
+
+    public GPS(float _updateFrequency, Transform _transform)//, int[,] _signalMap)
+    {
+        updateFrequency = _updateFrequency;
+        //signalMap = _signalMap;
+        coordinates = new Vector3();
+        orientation = new Quaternion();
+        accuracy = 0.2f;
+        transform = _transform;
+        time_real = Time.time;
+    }
+
+    public string GetGPSData()
+    {
+        string data = "X: " + coordinates.x.ToString() + "m Y: " + coordinates.y.ToString() + "m Z: " + coordinates.z.ToString() + "m Time: " + time_real.ToString() + "s";
+        return data;
+    }
+
+
+    public void UpdateState()
+    {
+        UpdateCoordinate();
+        UpdateOrientation();
+        UpdateTime();
+    }
+
+
+    private void UpdateCoordinate()
+    {
+        coordinates = transform.position + Vector3.one * GetNoise();
+    } 
+
+    private void UpdateOrientation()
+    {
+        orientation = transform.rotation;
+    }
+
+    private void UpdateTime()
+    {
+        time_real = Time.time;
+    }
+
+    private float GetNoise()
+    {
+        float noise = Random.Range(-accuracy / 2f, accuracy / 2f);
+        return noise;
+    }
+
+
+    
+
+}
+
+
---
+++ AVA/Assets/Scripts/Sensors/_GPS.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _GPS : MonoBehaviour
+{
+    private GPS GPS01;
+    private Transform transform;
+    public bool active;
+    private bool active_prev;
+    private IEnumerator activeGPS;
+    
+
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+        GPS01 = new GPS(0.5f, transform);
+        active = active_prev = false;
+        activeGPS = ActiveGPS(GPS01.updateFrequency);
+    }
+
+    public void OnGUI()
+    {
+        if (active != active_prev)
+        {
+            StartGPS(active);
+            active_prev = active;
+        }
+
+    }
+
+    private void StartGPS(bool _active)
+    {
+        active = _active;
+        if (active)
+        {
+            //Start Coroutine
+            StartCoroutine(activeGPS);
+            Debug.Log("Come on MadsR!");
+
+        }
+        else
+        {
+            //Stop coroutine
+            StopCoroutine(activeGPS);
+            Debug.Log("Chill the fuck down.");
+
+        }
+    }
+    private IEnumerator ActiveGPS(float _updateFrequency)
+    {
+        while (active)
+        {
+            GPS01.UpdateState();
+            Debug.Log(GPS01.GetGPSData());
+            yield return new WaitForSeconds(1f / _updateFrequency);
+        }
+
+    }
+}
