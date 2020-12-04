39aa4c2f393c8bd88eaab7fc82502515a3a67f8f
Supremeah <agerskov1994@gmail.com>
2020-12-04 17:28:45 +0100

    Cinematic Camera and grass size adjustment

---
+++ AVA/Assets/Scripts/Camera/CinematicCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinematicCamera : MonoBehaviour
+{
+    private Rigidbody cinematicRigidbody;
+    public Vector3 worldVelocity;
+    public Vector3 worldAngularVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cinematicRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cinematicRigidbody.velocity = worldVelocity;
+        cinematicRigidbody.angularVelocity = worldAngularVelocity;
+    }
+}
